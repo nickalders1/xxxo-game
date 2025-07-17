2df3820a9dd51fbfd0f0f6e454b8c0f503ad6bfa
Odsko <57218073+nickalders1@users.noreply.github.com>
2025-07-17 23:09:27 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    allowedDevOrigins: ["https://xxxo.bothosts.com"],
+  },
 };
 
 export default nextConfig;
